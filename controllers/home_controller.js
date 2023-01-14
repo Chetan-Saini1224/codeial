eac99f8d66e85afca00bb2b4d7b3a7c32d80df61
Chetan Saini <cs75198404@gmail.com>
2023-01-14 15:01:00 +0530

    add a controller and rounter for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,28 @@
+//module.export.actionName = function(req,res){}
+const fs = require('fs'); //not working path file
+
+module.exports.home = function(req,res)
+{
+    return res.send("<h1>This is home page</h1>");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port  = 8000;
 
+//use express router(no need to give /index)
+app.use('/',require('./routes'));
+
 
 app.listen(port,(err) =>{
 if(err)
 {
-    //callled interpolation
-    console.log(`Error in running the server: ${err}`);
+    console.log(`Error in running the server: ${err}`);    //callled interpolation `Error in running the server: ${err}`
 }
 console.log(`Server is running on Port ${port}`)
 });
